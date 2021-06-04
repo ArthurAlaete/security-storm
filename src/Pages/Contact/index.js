b185f0e554584906bb17f2060beb725a5aec07d5
ArthurAlaete <arthuralaetelopes@gmail.com>
2021-06-03 23:45:05 -0300

    Pagina contato: FormSide e iniciacao do ContactInfoSide

--- src/Pages/Contact/index.js
+++ src/Pages/Contact/index.js
@@ -1,28 +1,83 @@
 import React from 'react';
-
 import Header from '../../components/Header';
 import NavBanner from '../../components/NavBanner';
 import Footer from '../../components/Footer';
+import ImagemContato from '../../Assets/images/contato.png';
 
-import { Container } from './styles';
+import { 
+    Container, 
+    Content, 
+    FormSide, 
+    ContactInfoSide, 
+    Image,
+    TopInfo,
+    Line,
+    BottomInfo,
+    Contacts,
+    PhoneInfo,
+    PhoneIcon,
+    EmailInfo,
+    EmailIcon,
+    OfficeHourInfo,
+    ClockIcon,
+    SocialMedias,
+    FacebookIcon,
+    InstagramIcon,
+    TwitterIcon,
+} from './styles';
 
 const Contact = () => {
     return(
         <Container>
             <Header />
             <NavBanner currentPage="Contato" navPath="/contato" />
-            {/* <Content>
-                <Form>
-
-                </Form>
-                <SideContactInfo>
-                    
-                </SideContactInfo>
-            </Content> */}
+            <Content>
+                <FormSide>
+                    <form>
+                        <label>Nome(obrigatório)</label>
+                        <input type="text" name="nome" placeholder="Nome"/>
+                        <label>Email(obrigatório)</label>
+                        <input type="text" name="email" placeholder="Email"/>
+                        <label>Assunto</label>
+                        <input type="text" name="nome" placeholder="Assunto"/>
+                        <label>Sua Mensagem</label>
+                        <textarea placeholder="Sua mensagem"></textarea>
+                    </form>
+                    <button>Enviar</button>
+                </FormSide>
+                <ContactInfoSide>
+                    <TopInfo>
+                        <Image src={ImagemContato} alt="deu merda"/>
+                        <p>Por favor, preencha os campos ao lado com o máximo de informações para fácil entendimento</p>
+                    </TopInfo>
+                    <Line />
+                    <BottomInfo>
+                        <h2>Detalhes de contato</h2>
+                        <Contacts>
+                            <PhoneInfo>
+                                <PhoneIcon />
+                                <p>+55 84 9999-9999</p>
+                            </PhoneInfo>
+                            <EmailInfo>
+                                <EmailIcon />
+                                <p>securitystorm@gmail.com</p>
+                            </EmailInfo>
+                            <OfficeHourInfo>
+                                <ClockIcon />
+                                <p>Nosso Expediente: Seg - Sex 08:30 - 18:00</p>
+                            </OfficeHourInfo>
+                        </Contacts>
+                        <SocialMedias>
+                            <FacebookIcon />
+                            <InstagramIcon />
+                            <TwitterIcon />
+                        </SocialMedias>
+                    </BottomInfo>
+                </ContactInfoSide>
+            </Content>
             <Footer />
         </Container>
     )
 }
-
 
 export default Contact;
--- src/Pages/Contact/styles.js
+++ src/Pages/Contact/styles.js
@@ -1,11 +1,155 @@
 import styled from 'styled-components';
+import { 
+    Phone, 
+    EmailOutline, 
+    Clock,
+    Facebook,
+    Instagram,
+    Twitter
+} from '../../styles/Icons';
 
 export const Container = styled.div`
     display: grid;
-    grid-template-rows: 100px 130px 250px;
+    grid-template-rows: 100px 130px auto 250px;
     grid-template-areas:
         "header header"
         "navBanner navBanner"
+        "content content"
         "footer footer"
     ;
 `;
+
+export const Content = styled.div`
+    grid-area: content;
+
+    display: grid;
+    grid-template-columns: 60% 1fr;
+    grid-template-areas: 
+        "formSide contactInfoSide"
+    ;
+`; 
+
+export const FormSide = styled.div`
+    grid-area: formSide;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    padding: 80px;
+    border: solid 1px black;
+    
+    form {
+        display: flex;
+        flex-direction: column;
+
+        label {
+            font-size: 20px;
+            font-weight: 500;
+            /* color: 'black'; */
+            margin-bottom: 18px;
+        }
+
+        input, textarea {
+            border: solid 1px var(--primary);
+            border-radius: 8px;
+            padding: 10px;
+            width: 520px;
+            margin-bottom: 20px;
+        }
+    }
+    
+    button {
+        width: 120px;
+        margin-top: 20px;
+        padding: 10px 30px;
+        border-radius: 8px;
+        color: var(--white);
+        background-color: var(--primary);
+        font-weight: bold;
+        text-align: center;
+    }
+
+    button:hover {
+        background-color: var(--tertiary);
+    }
+`;
+
+export const ContactInfoSide = styled.div`
+    grid-area: contactInfoSide;
+    display: flex;
+    flex-direction: column;
+    padding: 75px;
+`;
+
+export const TopInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+
+    p {
+        color: var(--tertiary);
+        font-size: 16px;
+    }
+`;
+
+export const Image = styled.img`
+    width: 100%;
+    height: 150px;
+    margin-bottom: 20px;
+    border-radius: 8px;
+`;
+
+export const Line = styled.hr`
+    width: 100%;
+`;
+
+export const BottomInfo = styled.div`
+
+`;
+
+export const Contacts = styled.div`
+
+`;
+
+export const PhoneInfo = styled.div`
+
+`;
+
+export const PhoneIcon = styled(Phone)`
+
+`;
+
+export const EmailInfo = styled.div`
+
+`;
+
+export const EmailIcon = styled(EmailOutline)`
+
+`;
+
+export const OfficeHourInfo = styled.div`
+
+`;
+
+export const ClockIcon = styled(Clock)`
+
+`;
+
+export const SocialMedias = styled.div`
+
+`;
+
+export const FacebookIcon = styled(Facebook)`
+
+`;
+
+export const InstagramIcon = styled(Instagram)`
+
+`;
+
+export const TwitterIcon = styled(Twitter)`
+
+`;
+
+
