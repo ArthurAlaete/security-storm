562f45ae0320603d98b692921370384486adaf79
ArthurAlaete <arthuralaetelopes@gmail.com>
2021-07-03 22:44:53 -0300

    implementacao do component ScrollToTop

---
+++ src/components/ScrollToTop/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+const ScrollToTop = ({ history }) => {
+    
+    React.useEffect(() => {
+        const unlisten = history.listen(() => {
+            window.scrollTo(0, 0);
+        });
+        
+        return () => {
+            unlisten();
+        }
+
+    }, [history]);
+
+    return (null);
+}
+
+export default withRouter(ScrollToTop);
