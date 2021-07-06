979f19ae12fbc28ab4cbc5f7880c403aca342e7a
ArthurAlaete <arthuralaetelopes@gmail.com>
2021-07-05 22:05:33 -0300

    ajustes diretorios

--- src/styles/Icons.js
+++
@@ -1,7 +0,0 @@
-export { Phone } from '@styled-icons/boxicons-regular';
-export { EmailOutline, ArrowIosDownwardOutline } from '@styled-icons/evaicons-outline';
-export { Clock, Twitter, Facebook, Instagram } from '@styled-icons/bootstrap';
-export { Eye, Server } from '@styled-icons/fa-solid';
-export { Spy } from '@styled-icons/remix-fill';
-export { People, AlertTriangle } from '@styled-icons/evaicons-solid';
-export { Checklist } from '@styled-icons/octicons'
